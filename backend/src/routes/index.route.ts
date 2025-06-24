3e600041642c061994ddb9e210da7ed7bbe784f6
kingsleycj <primekings.kc@gmail.com>
2025-06-24 18:13:47 +0100

    routing guide

--- backend/src/routes/index.route.ts
+++ backend/src/routes/index.route.ts
@@ -7,7 +7,8 @@ router.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// 
+// Register the volunteer routes
+// This will handle all routes starting with /volunteer
 router.use("/volunteer", volunteerRoutes);
 
 export default router;
